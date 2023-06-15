fcff38049796571ecef0220354b47134df31d43a
andersonbosa <andersonbosa0@gmail.com>
2023-06-15 20:59:50 -0300

    Checkpoint

--- src/components/Resume/ResumeProjectItem.vue
+++ src/components/Resume/ResumeProjectItem.vue
@@ -10,16 +10,11 @@ defineProps({
 </script>
 
 <template>
-  <div class="project_item_wrapper w-full h-full m-1">
-    <div
-      class="project_item_card block w-full h-full rounded-lg bg-white shadow-lg dark:bg-neutral-700 dark:shadow-black/30"
-    >
-      <div
-        class="project_item_top_border h-1 overflow-hidden rounded-t-lg bg-primary"
-      ></div>
-
-      <div class="project_item_card_content p-6 w-full h-full">
-        <h4 class="mb-4 text-xl font-semibold">{{ project.name }}</h4>
+  <div class="project_item_wrapper">
+    <div class="project_item_card">
+      <div class="project_item_top_border"></div>
+      <div class="project_item_card_content">
+        <h4>{{ project.name }}</h4>
         <hr />
         <p class="mt-4">
           <img class="inline-block h-7 w-7 pr-2" src="/images/quote_symbol.svg" />
@@ -32,14 +27,20 @@ defineProps({
 
 <style scoped lang="postcss">
 .project_item_wrapper {
+  @apply w-full h-full m-1;
 }
 .project_item_card {
+  @apply block w-full h-full rounded-lg bg-white shadow-lg dark:bg-neutral-700 dark:shadow-black/30;
 }
-.project_item_top_bo rder {
+.project_item_top_border {
+  @apply h-1 overflow-hidden rounded-t-lg bg-primary;
 }
 .project_item_card_content {
   word-wrap: break-word;
-}
+  @apply p-6 w-full h-full;
 
-/* ... */
+  h4 {
+    @apply mb-4 text-xl font-semibold;
+  }
+}
 </style>
