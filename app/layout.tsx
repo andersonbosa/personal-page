ef69c284b09af18e9ec202194cedd7cb93fba489
andersonbosa <andersonbosa0@gmail.com>
2024-02-01 17:33:40 -0300

    feat: create nextjs app
    setup hello world project

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Anderson Bosa | Software Engineer - Penetration Tester - Bachelor in Information Systems",
+  description: "Anderson has been an active software engineer since 2019. Enthusiast at Leadership, Ethical Hacking and Red Teams Operations, Anderson seeks excellence in his career by focusing on interdisciplinary learning.",
+}
+
+export default function RootLayout ({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
