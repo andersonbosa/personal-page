459f637ba2d7ca6b0dc0deb41ca74547b7f00705
andersonbosa <andersonbosa0@gmail.com>
2024-02-05 00:26:03 -0300

    add contacts tree

--- constants.ts
+++ constants.ts
@@ -26,3 +26,43 @@ export const AVAIABLE_LANGUAGES: LanguageItem[] = [
 export const AVAILABLE_THEMES = [
   'pastel', 'retro', 'dracula', 'dark'
 ]
+
+interface MyContact {
+  label: string
+  lucideIcon: string
+  contact: string
+  hrefBuilder: (input: any) => string
+}
+
+export const MY_CONTACTS: MyContact[] = [
+  {
+    label: 'WhatsApp',
+    lucideIcon: 'Phone',
+    contact: '48988142547',
+    hrefBuilder: (phoneNumber: string) => `https://api.whatsapp.com/send/?phone=55${phoneNumber}&text=Hello%20Anderson!&type=phone_number&app_absent=1`,
+  },
+  {
+    label: 'Email',
+    lucideIcon: 'Mail',
+    contact: 'andersonbosa0@gmail.com',
+    hrefBuilder: (emailAddress: string) => `mailto://${emailAddress}`,
+  },
+  {
+    label: 'Linkedin',
+    lucideIcon: 'Linkedin',
+    contact: 'andersonbosa',
+    hrefBuilder: (username: string) => `https://linkedin.com/in/${username}`,
+  },
+  {
+    label: 'GitHub',
+    lucideIcon: 'Github',
+    contact: 'andersonbosa',
+    hrefBuilder: (username: string) => `https://github.com/${username}`,
+  },
+  {
+    label: 'Twitter',
+    lucideIcon: 'Twitter',
+    contact: 't4inha',
+    hrefBuilder: (username: string) => `https://twitter.com/${username}`,
+  }
+]
