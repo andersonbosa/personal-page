fff024d98ed736ac06ae9d98ac58bb53f249edb7
andersonbosa <andersonbosa0@gmail.com>
2024-02-19 21:23:41 -0300

    feat: add Hero molecule and update styles
    - Introduces a new Hero molecule for the homepage.
    - Updates styles for Navbar and ConfigFloatingButton components.

--- components/molecules/AboutMe.tsx
+++ components/molecules/AboutMe.tsx
@@ -6,29 +6,35 @@ interface AboutMeProps { }
 
 const AboutMe: React.FC<AboutMeProps> = ({ }) => {
   return (
-    <section
+    <div
       id="afterTheHeroHeader"
-      className="min-h-screen -- flex flex-wrap -- text-neutral-content"
+      className="min-h-screen w-full -- flex items-center justify-center content-start -- flex-wrap lg:flex-nowrap -- p-8"
     >
-      <div className="flex justify-center items-center w-full md:w-1/2">
+      <div className="w-full lg:w-[40%] -- flex justify-center items-center shrink-1 ">
         <Image
-          className="mask mask-squircle"
+          className="mask mask-squircle object-cover"
           src={'https://github.com/andersonbosa.png'}
           height={321}
           width={321}
+          style={{ color: 'transparent' }}
           alt="Perfil image"
         />
       </div>
 
-      <div className="w-full md:w-1/2 text-center py-8">
-        <h1 className="text-4xl font-bold mb-4">Olá! Eu sou o Anderson</h1>
-        <p className="text-justify mb-8">
-          Mollit ad officia cillum laborum veniam voluptate cillum. Cillum
-          officia ad non fugiat esse. Tempor minim nulla nostrud esse esse
-          officia ut incididunt laborum non mollit nisi laborum elit.
-        </p>
+      <div className="w-full lg:w-1/2 lg:max-w-[600px] -- basis-9/12 grow -- text-center  ">
+        <h1 className="text-4xl font-bold -- text-primary">Olá! Eu sou o Anderson</h1>
+        <br />
+        <section className="text-justify">
+          <p >
+            Em minha função mais recente na Conviso como Engenheiro de Software Backend, onde passei um ano e meio, aprimorei minhas habilidades de resolução de problemas, otimização de serviços, desenvolvimento de APIs e liderança na manutenção de sistemas críticos.
+          </p>
+          <br />
+          <p>
+            Anteriormente, ainda na Conviso como Analista de Segurança, eu adquiri experiência em auditorias automatizadas e manuais de sistemas web, móveis e redes. Também conduzi de forma solo como Penetration Tester projetos para amadurecer a maturidade da segurança das aplicações de nossos usuários, realizando avaliação das vulnerabilidades encontradas e auxiliando na correção ou mitigação dessas.
+          </p>
+        </section>
       </div>
-    </section>
+    </div>
   )
 }
 
